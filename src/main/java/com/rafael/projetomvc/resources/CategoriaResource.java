2429b480e978ca89ecf42938036d219b5696a364
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-03-30 21:04:23 -0300

    Banco H2 e criação automatica da tabela Categoria

--- src/main/java/com/rafael/projetomvc/resources/CategoriaResource.java
+++ src/main/java/com/rafael/projetomvc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.rafael.projetomvc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafael.projetomvc.dominio.Categoria;
 
 @RestController
 @RequestMapping(value ="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-	  return "Funcionou";
+	public List<Categoria> listar() {
+	  Categoria c1 = new Categoria(1,"Informatica");
+	  
+	  Categoria c2 = new Categoria(2, "Escritorio");
+	  
+	  List<Categoria> lista = new ArrayList<Categoria>();
+	  lista.add(c1);
+	  lista.add(c2);
+	  
+	  
+	  return lista;
 	}
 	
 
