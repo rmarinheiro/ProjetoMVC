057127c91530fa06b90ec6ab51bf3438f6db121a
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-03-30 23:57:53 -0300

    Criando Instanciação

--- src/main/java/com/rafael/projetomvc/ProjetoMvcApplication.java
+++ src/main/java/com/rafael/projetomvc/ProjetoMvcApplication.java
@@ -1,13 +1,35 @@
 package com.rafael.projetomvc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoMvcApplication {
+import com.rafael.projetomvc.dominio.Categoria;
+import com.rafael.projetomvc.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoMvcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoMvcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+		
 	}
 
 }
