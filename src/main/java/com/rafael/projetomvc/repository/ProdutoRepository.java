0ba7121b898ce9e90b940abaf6af83a483053488
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-04-21 23:39:11 -0300

    Busca de pedidos e Nome por Categoria

--- src/main/java/com/rafael/projetomvc/repository/ProdutoRepository.java
+++ src/main/java/com/rafael/projetomvc/repository/ProdutoRepository.java
@@ -1,11 +1,23 @@
 package com.rafael.projetomvc.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rafael.projetomvc.dominio.Categoria;
 import com.rafael.projetomvc.dominio.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> seach(@Param("nome") String nome, @Param("categorias")List<Categoria> categorias, Pageable page);
 
 }
--- src/main/java/com/rafael/projetomvc/services/ProdutoService.java
+++ src/main/java/com/rafael/projetomvc/services/ProdutoService.java
@@ -1,21 +1,38 @@
 package com.rafael.projetomvc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
 
 import com.rafael.projetomvc.dominio.Categoria;
 import com.rafael.projetomvc.dominio.Produto;
+import com.rafael.projetomvc.repository.CategoriaRepository;
 import com.rafael.projetomvc.repository.ProdutoRepository;
 
+@Service
 public class ProdutoService {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public Produto  find(Integer cod) {
 		Optional<Produto> obj = produtoRepository.findById(cod);
 		return obj.orElse(null);
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids,Integer page , Integer linesPage, String orderBy,String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return produtoRepository.seach(nome,categorias,pageRequest);
+		
 	}
 
 }
