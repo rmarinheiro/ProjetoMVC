2a52c72578750357dab3ddb6c003f9321b7a1ac8
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-05-08 00:13:32 -0300

    implementando autorização

--- src/main/java/com/rafael/projetomvc/config/SecurityConfig.java
+++ src/main/java/com/rafael/projetomvc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.rafael.projetomvc.security.JWTAuthenticationFilter;
+import com.rafael.projetomvc.security.JWTAuthorizationFilter;
 import com.rafael.projetomvc.security.JWTUtil;
 
 @Configuration
@@ -55,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		 
 	}
