2a915be5bf4dbb458f31c3c6dc4222f98d3cfebb
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-05-14 16:20:21 -0300

    Recuperação de Senha

--- src/main/java/com/rafael/projetomvc/config/SecurityConfig.java
+++ src/main/java/com/rafael/projetomvc/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	public static final String [] PUBLIC_MATCHERS_POST = {
 			"/clientes/**",
+			"/auth/forgot/**"
 			
 	};
 	
@@ -62,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
 		.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
-		.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_POST).permitAll()
+		.antMatchers(HttpMethod.POST,PUBLIC_MATCHERS_POST).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
